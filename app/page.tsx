e5e4bb8021061dc200e314c3588145e168533bd7
Hemanth <hemanth6339@gmail.com>
2025-10-21 14:06:24 +0530

    sign-in redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import Header from "../components/header";
-
+import { redirect } from "next/navigation";
 export default function Home() {
+  redirect('/sign-in');
   return (
     <>
     <Header/>
