f9249aa44f202c25543a5eb630d006a89893b783
Hemanth <hemanth6339@gmail.com>
2025-10-21 14:17:41 +0530

    sig

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,8 +1,10 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.redirect(new URL('/sign-in', request.url));
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/signin", request.url));
   }
+
+  return NextResponse.next();
 }
